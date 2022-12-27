ef09aabababf72ce8bc06efcfe5f20d561049994
Eliot Valdes <eliottvaldes@hotmail.com>
2022-12-27 13:00:29 -0600

    upd -> create new elements to show qatarTime

--- 02-React/01-first-app/src/App.js
+++ 02-React/01-first-app/src/App.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './App.css';
 
 function App() {
+
+  let localTime = new Date()  
+  // get qatar time using the difference of 9 hours
+  let qatarTime = new Date(localTime.getTime() + (3600000 * 9))
+
+
   const root = ReactDOM.createRoot(document.getElementById('root'));
   const element = (
     <div className="App">
@@ -10,6 +16,7 @@ function App() {
         <h1> The current world time is: </h1>
         {/* show te current hour */}
         <h3> México: {new Date().toLocaleTimeString('es-MX')} </h3>
+        <h3> Qatar (local): {qatarTime.toLocaleTimeString('es-MX')} </h3>
         <h3> United Stated: {new Date().toLocaleTimeString('en-US')} </h3>
         <h3> Korea: {new Date().toLocaleTimeString('ko-KR')} </h3>
         <h3> Qatar: {new Date().toLocaleTimeString('ar-QA')} </h3>
