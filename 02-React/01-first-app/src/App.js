ac72fbf25990d3d456ffbf2d1ace12020d6ddf3b
Eliot Valdes <eliottvaldes@hotmail.com>
2022-12-27 12:48:17 -0600

    upd -> render all aplication inside function

--- 02-React/01-first-app/src/App.js
+++ 02-React/01-first-app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
 function App() {
-  return (
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const element = (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> The current world time is: </h1>
+        {/* show te current hour */}
+        <h3> México: {new Date().toLocaleTimeString('es-MX')} </h3>
+        <h3> United Stated: {new Date().toLocaleTimeString('en-US')} </h3>
+        <h3> Korea: {new Date().toLocaleTimeString('ko-KR')} </h3>
+        <h3> Qatar: {new Date().toLocaleTimeString('ar-QA')} </h3>
       </header>
     </div>
   );
+
+  // render the app
+  root.render(element)
 }
 
 export default App;
