ced3ba6167430be0bd15cf34af3253c3a2b78845
Eliot Valdes <eliottvaldes@hotmail.com>
2022-12-27 03:37:17 -0600

    upd -> create hello world using http request

--- 00-Prework/01-helloWorld/index.js
+++ 00-Prework/01-helloWorld/index.js
@@ -1,1 +1,23 @@
 console.log('Hello world')
+
+// create fist basic api
+const http = require('http')
+
+// define the host and port
+const host = '127.0.0.1'
+const port = '3000'
+
+// create server using http req
+const server = http.createServer((req, res) => {
+    // define the response state
+    res.statusCode = 200
+    // define the response header
+    res.setHeader('Content-Type', 'text/plain')
+    // define the response body
+    res.end('Hello World from http req')
+})
+
+// start server
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`)
+})
