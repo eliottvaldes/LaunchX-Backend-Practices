e7f7f2338d05cba665ce8d973f2d73f2e232c56b
Eliot Valdes <eliottvaldes@hotmail.com>
2022-12-27 19:22:33 -0600

    upd -> implement logic at TaskList comp

--- 02-React/02-todo-app/src/components/TaskList.jsx
+++ 02-React/02-todo-app/src/components/TaskList.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import Checkbox from './Checkbox'
 
-const TaskList = () => {
+const TaskList = (props) => {
+
+    const {list, setList} = props    
+
+    // manage onChangeStatus
+    const onChangeStatus = (e) => {
+        const {name, checked} = e.target
+        const updateList = list.map(item => ({
+            ...item, 
+            done: item.id === name ? checked : item.done
+        }))
+        setList(updateList)
+    }
+
+    const onClickRemoveItem = (e) => {
+        const updateList = list.filter(item => !item.done)
+        setList(updateList)
+    }
+
+    const checkbox = list.map(item => (
+        <Checkbox key={item.id} data={item} onChange={onChangeStatus} />
+    ))
+
+
     return (
-        <div>
-            TaskList component
-            <Checkbox/>
+        <div className='todo-list'>
+            {list.length ? checkbox : 'There is any task'}
+
+            {list.length ? (
+                <p>
+                    <button className='button-blue' onClick={onClickRemoveItem}> Delete</button>
+                </p>
+            ) : null}
         </div>
     )
 }
