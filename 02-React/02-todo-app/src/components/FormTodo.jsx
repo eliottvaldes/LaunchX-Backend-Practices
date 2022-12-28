13da4919a8b61efc900e1d0a46c1fb8fd704f3f8
Eliot Valdes <eliottvaldes@hotmail.com>
2022-12-27 18:46:45 -0600

    upd -> implement logic to FormTodo comp

--- 02-React/02-todo-app/src/components/FormTodo.jsx
+++ 02-React/02-todo-app/src/components/FormTodo.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const FormTodo = () => {
+// get the props
+const FormTodo = (props) => {
+
+    // using use state to get variables
+    const [description, setDescription] = useState('')
+    // define a method setItem from props
+    const {setItem} = props
+
+    // function to prevent default behavior at form submit
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(description)
+
+        // set new Item
+        
+        setItem({
+            done: false,
+            id: (+new Date()).toString(),
+            description
+        })
+
+        // clear description
+        setDescription('')
+    }
+
     return (
         <div>
-            Form Todo Coponent
+            {/* implement prevent default f' */}
+            <form onSubmit={handleSubmit}>
+                <div className='todo-list'>
+                    <div className='input'>
+                        <input 
+                        type='text'
+                        className='input'
+                        value={description}
+                        // implement an event to setDescription
+                        onChange={e => setDescription(e.target.value)}
+                        />
+                        <button
+                        className='button-blue'
+                        disabled={!description}
+                        >
+                            Add task
+                        </button>
+
+                        
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
