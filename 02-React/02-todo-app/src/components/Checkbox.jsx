9d1af4914ea85843625826714e05f6290a66e23b
Eliot Valdes <eliottvaldes@hotmail.com>
2022-12-27 20:43:21 -0600

    upd -> show data from each task

--- 02-React/02-todo-app/src/components/Checkbox.jsx
+++ 02-React/02-todo-app/src/components/Checkbox.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Checkbox = () => {
+// get the data inside the comp
+const Checkbox = (props) => {
+
+    const {data, onChange} = props
+
+    console.log(onChange, data.id)
+
+    const changeStatus = (e) => {
+        data.done = !data.done
+        console.log(data.done)
+    }
+
     return (
         <div>
-            Checkbox component
+            <p className={data.done ? 'task-done' : 'task-pending'}> 
+                {data.description} 
+            </p>
+            <input type="checkbox" onClick={changeStatus} value={data.id} name={data.description} />
+            <hr/>
         </div>
     )
 }
