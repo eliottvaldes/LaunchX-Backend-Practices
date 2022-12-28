e32820b5c879088b313a2331caf85095da7c97ce
Eliot Valdes <eliottvaldes@hotmail.com>
2022-12-27 18:54:03 -0600

    upd -> create logic to connect components at Container.jsx

--- 02-React/02-todo-app/src/components/Container.jsx
+++ 02-React/02-todo-app/src/components/Container.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FormTodo from './FormTodo'
 import TaskList from './TaskList'
 
 const Container = () => {
+
+    // craete variables useing useState
+    const [list, setList] = useState([])
+
+    // craete a f' to save item in list
+    const setItem = (setItem) => {
+        setList([list, setItem])
+    }
+
     return (
         <div>
-            <FormTodo/>
-            <TaskList/>
+            {/* pass the setItem props to children */}
+            <FormTodo setItem={setItem} />
+            <TaskList list={list} setList={setList} />
         </div>
     )
 }
